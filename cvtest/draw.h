8af7effae0a14755e712bdc2570b8911759546cd
alexheqingli <alex.heqingli@gmail.com>
2014-02-15 13:42:26 -0800

    Separated out a cpp file for draw

---
+++ cvtest/draw.h
@@ -0,0 +1,19 @@
+//
+//  draw.h
+//  cvtest
+//
+//  Created by Alex Li on 2/15/14.
+//  Copyright (c) 2014 Alex Li. All rights reserved.
+//
+
+#ifndef __cvtest__draw__
+#define __cvtest__draw__
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#endif /* defined(__cvtest__draw__) */
+
+
+// Function prototype for detecting and drawing an object from an image
+void detect_and_draw( IplImage* image );
